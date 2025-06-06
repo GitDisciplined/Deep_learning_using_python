84e0bccd0d339061d570ed6ccce76452d45b6340
GitDisciplined <68583436+GitDisciplined@users.noreply.github.com>
2025-06-06 07:40:18 +0530

    Add files via upload

---
+++ cross_entropy.py
@@ -0,0 +1,17 @@
+
+import numpy as  np
+import math
+
+
+
+p= [.7,.1,.2]
+
+
+target=[1,0,0]
+
+
+loss=-(target[0]*math.log(p[0])+ target[1]*math.log(p[1])+
+       target[2]*math.log(p[2]))
+
+print(loss)
+
